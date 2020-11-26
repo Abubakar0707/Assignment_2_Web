c6475dcdef81b15b7515376e82d2caa5dd3b1439
Muhammad Abubakar Ijaz <abubakar0707@gmail.com>
2020-11-26 19:16:17 +0500

    Update calorie.js

--- calorie.js
+++ calorie.js
@@ -1,51 +1,50 @@
-$(function () {
-    $("#form-id").submit(function () {
-      return false;
+$(function(){
+    $("#form-id").submit(function(){
+        return false;
     });
-    $("#carbTotal").html(sumColumn(2));
-    $("#calTotal").html(sumColumn(3));
-  
-    $("#add").click(function () {
-      var sel = document.getElementById("scripts");
-      var opt = sel.options[sel.selectedIndex];
-      var carbss = opt.getAttribute("carbs");
-      var cals = opt.getAttribute("cal");
-      var food = document.getElementById("food");
-      food.innerHTML =
-        food.innerHTML +
-        `
-        <tr>
-        <th scope="row">${opt.text}</th>
-        <td carbsValue="${carbss}">${carbss}</td>
-        <td calsValue="${cals}">${cals}</td>
-        <td><button class="btn btn-primary" onclick='delItem(event)'>Del</button></td>
-      </tr>`;
-  
-      console.log(opt.value);
-      console.log(opt.text);
-      console.log(carbss);
+    $("#carbtotal").html(sumColumn(2));
+    $("#caltotal").html(sumColumn(3));
+
+    $("#add").click(function(){
+        var itm = document.getElementById("items");
+        var option = itm.options[itm.selectedIndex];
+        var carbbs = option.getAttribute("carbs");
+        var cal = option.getAttribute("cal");
+        var calfood = document.getElementById("calfood");
+        calfood.innerHTML=
+        calfood.innerHTML+ `<tr>
+        <th scope="row">${option.text}</th>
+        <td carbbsValue="${carbbs}">${carbbs}</td>
+        <td calValue="${cal}">${cal}</td>
+        <td><button class"btn btn-danger btn-sm" type"button" onclick='delitem(event)'>Delete</button></td>
+        </tr>`;
+        console.log(option.value);
+        console.log(option.text);
+        console.log(carbbs);
     });
     $("#calculate").click(calculate);
-  });
-  
-  function delItem(e) {
+});
+
+function delitem(e)
+{
     var btn = e.target;
     var td = btn.parentNode;
     var tr = td.parentNode;
     tr.parentNode.removeChild(tr);
-    $("#carbTotal").html(0);
-    $("#calTotal").html(0);
-  }
-  
-  function calculate() {
-    $("#carbTotal").html(sumColumn(2));
-    $("#calTotal").html(sumColumn(3));
-  }
-  function sumColumn(index) {
+    $("#carbtotal").html(sumColumn(0));
+    $("#caltotal").html(sumColumn(0));
+}
+
+function calculate()
+{
+    $("#carbtotal").html(sumColumn(2));
+    $("#caltotal").html(sumColumn(3));
+}
+
+function sumColumn(index) {
     var total = 0;
     $("td:nth-child(" + index + ")").each(function () {
       total += parseInt($(this).text(), 10) || 0;
     });
     return total;
   }
-  
